30a18f090d143c2194b179575dee43fb1c19ec0f
Jeremy Horgan <jeremyhorgan@hotmail.com>
2019-06-20 00:05:43 +0100

    Initial files

---
+++ PrismWpfApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace PrismWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
---
+++ PrismWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace PrismWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
